6ad2654a752bb4f269b700d8157118137cc6af4b
Alexey <alex3122007@yandex.ru>
2022-12-18 12:02:01 +0300

    homework

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int i = a%2;
-if (i == 1)   Console.WriteLine("Число не четное");
+
+if (a%2 == 1)   
+     Console.WriteLine("Число не четное");
 else Console.WriteLine("Число четное");
